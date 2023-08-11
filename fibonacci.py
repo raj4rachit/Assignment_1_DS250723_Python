dfb349703ff1d23d8c1317564bd625ab4c124e99
tbsrachitpatel <rpatel@snapplify.com>
2023-08-11 21:03:29 +0530

    correct result

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,9 @@
-limit = 50
+limit = int(input("Enter the Number for generating Fibonacci series: "))
 fib_series = [0, 1]
+series = ''
 while fib_series[-1] + fib_series[-2] <= limit:
     fib_series.append(fib_series[-1] + fib_series[-2])
 
-print(f"Fibonacci series up to {limit}: {fib_series}")
+fib_series = fib_series[1:]
+fibonacci_string = ' '.join(map(str, fib_series))
+print(f"Fibonacci series between 0 to {limit}: {fibonacci_string}")
