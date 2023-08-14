04b1cb868720798bde41beefefa2648fc8e62250
tbsrachitpatel <rpatel@snapplify.com>
2023-08-14 11:28:11 +0530

    Odd Even count

--- odd_even_count.py
+++ odd_even_count.py
@@ -1,22 +1,22 @@
-def count_even_odd(numbers):
-    even_count = 0
-    odd_count = 0
+def countOddEvenNumbers(numbers):
+    evenCount = 0
+    oddCount = 0
 
     for num in numbers:
         if num % 2 == 0:
-            even_count += 1
+            evenCount += 1
         else:
-            odd_count += 1
-    return even_count, odd_count
+            oddCount += 1
+    return evenCount, oddCount
 
 try:
     num_series = input("Enter a series of numbers separated by spaces: ")
     num_list = [int(x) for x in num_series.split()]
 
-    even_count, odd_count = count_even_odd(num_list)
+    evenCount, oddCount = countOddEvenNumbers(num_list)
 
-    print("Number of even numbers:", even_count)
-    print("Number of odd numbers:", odd_count)
+    print("Number of even numbers: ", evenCount)
+    print("Number of odd numbers: ", oddCount)
 
 except ValueError:
     print("Invalid input. Please enter a valid series of numbers.")
