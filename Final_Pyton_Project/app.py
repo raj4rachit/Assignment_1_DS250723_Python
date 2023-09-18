4793f72dc3a22271d0fb829bf78e03d5b945ef74
tbsrachitpatel <rpatel@snapplify.com>
2023-09-18 19:12:15 +0530

    Final Project

--- Final_Pyton_Project/app.py
+++ Final_Pyton_Project/app.py
@@ -4,7 +4,9 @@ import re
 class FoodOrderingApp:
     def __init__(self):
         self.users = []
+        self.menu = []
         self.load_users()
+        self.load_menu()
         self.logged_in_user = None
 
     def load_users(self):
@@ -14,6 +16,14 @@ class FoodOrderingApp:
             self.users = user_data["users"]
         except FileNotFoundError:
             self.users = []
+
+    def load_menu(self):
+        try:
+            with open('menu.json', 'r') as menu_file:
+                menu_data = json.load(menu_file)
+            self.menu = menu_data["menu"]
+        except FileNotFoundError:
+            self.menu = []
 
     def save_users(self):
         with open('users.json', 'w') as users_file:
@@ -75,35 +85,72 @@ class FoodOrderingApp:
                 print("Logged in as:", self.logged_in_user["full_name"])
 
                 while True:
-                    print("\n:")
+                    print("\nPlace Order:")
                     print("1. Display Menu")
                     print("2. Take Orders")
                     print("3. Your Order History")
                     print("4. Update Profile")
                     print("5. Logout")
-                    choice = input("Enter your choice: ")
+                    your_choice = input("Enter your choice: ")
 
-                    if choice == '1':
-                        self.register_user()
-                    elif choice == '2':
-                        self.login()
-                    elif choice == '3':
+                    if your_choice == '3':
+                        self.user_order_history()
+                    elif your_choice == '4':
+                        self.update_profile()
+                    elif your_choice == '1':
                         self.display_menu()
-                    elif choice == '4':
+                    elif your_choice == '2':
                         if self.logged_in_user:
                             self.take_orders()
                         else:
                             print("Please log in first.")
-                    elif choice == '5':
-                        break
+                            return
+                    elif your_choice == '5':
+                        return
                     else:
                         print("Invalid choice. Please try again.")
 
         print("Login failed. Please check your email and password.")
 
+    def user_order_history(self):
+        if not self.logged_in_user:
+            print("Please log in to view your order history.")
+            return
+
+        print(f"Order History for {self.logged_in_user['full_name']} ({self.logged_in_user['email']}):")
+        # Implement order history retrieval and display logic here
+        # You may need to load order data from a separate JSON file
+
+    def update_profile(self):
+        if not self.logged_in_user:
+            print("Please log in to update your profile.")
+            return
+
+        print(f"Update Profile for {self.logged_in_user['full_name']} ({self.logged_in_user['email']}):")
+        # Implement profile update logic here, e.g., updating name, phone number, address, or password
+        new_full_name = input("New Full Name (press Enter to keep current): ")
+        new_phone_number = input("New Phone Number (press Enter to keep current): ")
+        new_address = input("New Address (press Enter to keep current): ")
+        new_password = input("New Password (press Enter to keep current): ")
+
+        # Update user profile data
+        if new_full_name:
+            self.logged_in_user['full_name'] = new_full_name
+        if new_phone_number:
+            self.logged_in_user['phone_number'] = new_phone_number
+        if new_address:
+            self.logged_in_user['address'] = new_address
+        if new_password:
+            self.logged_in_user['password'] = new_password
+
+        # Save updated user data
+        self.save_users()
+        print("Profile updated successfully.")
+
     def display_menu(self):
-        # Add your menu display logic here
-        pass
+        print("Menu:")
+        for item in menu:
+            print(f"{item['id']}. {item['name']} - ${item['price']}")
 
     def take_orders(self):
         # Add your order taking logic here
